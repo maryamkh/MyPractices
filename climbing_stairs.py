f651965514dc0182b385b09b0aaddb0fe7de8a1d
maryamkh <g.maryamkhosravi@gmail.com>
2023-02-26 22:43:04 -0600

    Update climbing_stairs.py

--- climbing_stairs.py
+++ climbing_stairs.py
@@ -10,7 +10,13 @@
 #3. 2 steps + 1 step
 '''
 This problem is Dynamic programming problem. We cal look at this problem in a tree (like brute forth) starting from stair zero and then each time taking either 1 step or 2 steps until we reach the 
-final stair (stair n). ===> A Tree has a recursive solution and since some of the steps are redundent we can haveme morization. 
+final stair (stair n). ===> A Tree has a recursive solution and since some of the steps are redundent we can haveme memoization.
+To reach step n, we have 2 ways:
+1- Stay on stair n-1 and take 1 step to reach stair n.
+2- Stay on stair n-2 and take 2 steps to reach stair n.
+======>number of ways to reach step n = number of ways to reach step n-1 + number of steps to reach step n-2.
+Time Complexity T(O) = O(n) ===> Since I usded "memoization" then each step is implemented only once and there is no redundent implementation.
+Space Complexity S(O) = O(n)
 '''
 class Solution:
     #top(n) = top (n-1) + top(n-2)
